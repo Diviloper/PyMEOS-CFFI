fa99e8ee897148a63fae7c5c0035c4fef97740eb
diviloper <vdiviloper@gmail.com>
2024-08-06 16:13:07 +0200

    Add more type conversions and clean conversion_map generation with conversion generation helpers

--- builder/objects.py
+++ builder/objects.py
@@ -16,9 +16,45 @@ class Conversion:
         self.c_to_p = c_to_p
 
 
+def expand_conversions(
+    c_type: str,
+    p_type: str,
+    p_to_c: Optional[Callable[[str], str]],
+    c_to_p: Optional[Callable[[str], str]],
+) -> Dict[str, Conversion]:
+    return {
+        c_type: Conversion(c_type, p_type, p_to_c, c_to_p),
+        f"{c_type} *": Conversion(f"{c_type} *", p_type, p_to_c, c_to_p),
+        f"const {c_type}": Conversion(f"const {c_type}", p_type, p_to_c, c_to_p),
+        f"const {c_type} *": Conversion(f"const {c_type} *", p_type, p_to_c, c_to_p),
+    }
+
+
+def int_conversions() -> Dict[str, Conversion]:
+    int_types = [
+        "int8",
+        "int16",
+        "int32",
+        "int64",
+        "uint8",
+        "uint16",
+        "uint32",
+        "uint64",
+        "uint8_t",
+    ]
+    return {
+        int_t: Conversion(
+            int_t, "int", lambda p_obj: f"_ffi.cast('{int_t}', {p_obj})", None
+        )
+        for int_t in int_types
+    }
+
+
 conversion_map: dict[str, Conversion] = {
     "void": Conversion("void", "None", None, None),
     "bool": Conversion("bool", "bool", None, None),
+    "int": Conversion("int", "int", None, None),
+    **int_conversions(),
     "double": Conversion("double", "float", None, None),
     "char *": Conversion(
         "char *",
@@ -32,60 +68,35 @@ conversion_map: dict[str, Conversion] = {
         lambda p_obj: f"{p_obj}.encode('utf-8')",
         lambda c_obj: f"_ffi.string({c_obj}).decode('utf-8')",
     ),
-    "text": Conversion(
+    **expand_conversions(
         "text",
         "str",
         lambda p_obj: f"cstring2text({p_obj})",
         lambda c_obj: f"text2cstring({c_obj})",
     ),
-    "text *": Conversion(
-        "text *",
-        "str",
-        lambda p_obj: f"cstring2text({p_obj})",
-        lambda c_obj: f"text2cstring({c_obj})",
-    ),
-    "const text": Conversion(
-        "const text",
-        "str",
-        lambda p_obj: f"cstring2text({p_obj})",
-        lambda c_obj: f"text2cstring({c_obj})",
-    ),
-    "const text *": Conversion(
-        "const text *",
-        "str",
-        lambda p_obj: f"cstring2text({p_obj})",
-        lambda c_obj: f"text2cstring({c_obj})",
-    ),
-    "int": Conversion("int", "int", None, None),
-    "int8": Conversion("int8", "int", lambda p_obj: f"_ffi.cast('int8', {p_obj})", None),
-    "int16": Conversion("int16", "int", lambda p_obj: f"_ffi.cast('int16', {p_obj})", None),
-    "int32": Conversion("int32", "int", lambda p_obj: f"_ffi.cast('int32', {p_obj})", None),
-    "int64": Conversion("int64", "int", lambda p_obj: f"_ffi.cast('int64', {p_obj})", None),
-    "uint8": Conversion("uint8", "int", lambda p_obj: f"_ffi.cast('uint8', {p_obj})", None),
-    "uint16": Conversion("uint16", "int", lambda p_obj: f"_ffi.cast('uint16', {p_obj})", None),
-    "uint32": Conversion("uint32", "int", lambda p_obj: f"_ffi.cast('uint32', {p_obj})", None),
-    "uint64": Conversion("uint64", "int", lambda p_obj: f"_ffi.cast('uint64', {p_obj})", None),
-    "uint8_t": Conversion("uint8_t", "int", lambda p_obj: f"_ffi.cast('uint8_t', {p_obj})", None),
-    "DateADT": Conversion("DateADT", "int", lambda p_obj: f"_ffi.cast('DateADT', {p_obj})", None),
-    "Timestamp": Conversion("Timestamp", "int", lambda p_obj: f"_ffi.cast('Timestamp', {p_obj})", None),
-    "TimestampTz": Conversion("TimestampTz", "int", lambda p_obj: f"_ffi.cast('TimestampTz', {p_obj})", None),
-    "TimestampTz *": Conversion(
-        "TimestampTz *",
+    "Timestamp": Conversion(
+        "Timestamp",
         "int",
-        lambda p_obj: f"_ffi.cast('TimestampTz *', {p_obj})",
+        lambda p_obj: f"_ffi.cast('Timestamp', {p_obj})",
         None,
     ),
-    "const TimestampTz": Conversion(
-        "const TimestampTz",
-        "int",
-        lambda p_obj: f"_ffi.cast('const TimestampTz', {p_obj})",
-        None,
+    **expand_conversions(
+        "TimestampTz",
+        "datetime",
+        lambda p_obj: f"datetime_to_timestamptz({p_obj})",
+        lambda c_obj: f"timestamptz_to_datetime({c_obj})",
     ),
-    "const TimestampTz *": Conversion(
-        "const TimestampTz *",
-        "int",
-        lambda p_obj: f"_ffi.cast('const TimestampTz *', {p_obj})",
-        None,
+    **expand_conversions(
+        "DateADT",
+        "date",
+        lambda p_obj: f"date_to_date_adt({p_obj})",
+        lambda c_obj: f"date_adt_to_date({c_obj})",
+    ),
+    **expand_conversions(
+        "Interval",
+        "timedelta",
+        lambda p_obj: f"timedelta_to_interval({p_obj})",
+        lambda c_obj: f"interval_to_timedelta({c_obj})",
     ),
     "TimeOffset": Conversion("TimeOffset", "int", lambda p_obj: f"_ffi.cast('TimeOffset', {p_obj})", None),
     "interpType": Conversion("interpType", "InterpolationType", None, None),
